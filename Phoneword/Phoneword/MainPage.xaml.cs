b141734edbae571ffce33b709fbe9efbc440ae63
Emily Hall <bibliomaniac@gmail.com>
2022-03-18 13:22:33 -0400

    Add project files.

---
+++ Phoneword/Phoneword/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    public class MainPage : ContentPage
+    {
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
+        string translatedNumber;
+
+        public MainPage()
+        {
+            this.Padding = new Thickness(20, 20, 20, 20); //sets padding for the page
+
+            StackLayout panel = new StackLayout
+            {
+                Spacing = 15 //establishes layout
+            };
+
+            panel.Children.Add(new Label //because this is stack layout, each subsequent panel.Children.Add is stacked top to bottom
+            {
+                Text = "Enter a Phoneword:",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
+            });
+
+            panel.Children.Add(phoneNumberText = new Entry
+            {
+                Text = "1-855-XAMARIN",
+            });
+
+            panel.Children.Add(translateButton = new Button
+            {
+                Text = "Translate",
+                
+            });
+
+            translateButton.Clicked += OnTranslate;
+
+            panel.Children.Add(callButton = new Button()
+            {
+                Text = "Call",
+                IsEnabled = false,
+            });
+
+            this.Content = panel;
+
+        }
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumberText.Text;
+            translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber); //calls the ToNumber function from the PhonewordTranslator module
+
+            if (!string.IsNullOrEmpty(translatedNumber))
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ Phoneword/Phoneword/PhonewordTranslator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core  
+{
+    public static class PhonewordTranslator
+    {
+        public static string ToNumber(string raw)
+        {
+            if (string.IsNullOrWhiteSpace(raw)) //condition for no or empty string
+                return null;
+
+            raw = raw.ToUpperInvariant(); //capitalizes all characters in the passed string
+
+            var newNumber = new StringBuilder();
+
+            foreach (var c in raw) //loops through each character i nthe uppercase string
+            {
+                if(" -0123456789".Contains(c))
+                    newNumber.Append(c); //checks if the character is already a number or separator and if so, adds to the newNumber array
+                else
+                {
+                    var result = TranslateToNumber(c); //calls the number translation function
+                    if (result != null)
+                        newNumber.Append(result); //if there's something there, adds to newNumber (like l 21)
+                    else
+                        return null; //if it's empty, returns nothing
+                }
+            }
+            return newNumber.ToString(); //takes the full list and returns it as a string
+        }
+        static bool Contains(this string keyString, char c)
+        {
+            return keyString.IndexOf(c) >= 0;
+        }
+
+        static readonly string[] digits =
+        {
+            "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ"
+        };
+
+        static int? TranslateToNumber(char c)
+        {
+            for (int i = 0; i < digits.Length; i++)
+            {
+                if (digits[i].Contains(c))
+                    return 2 + i;
+            }
+            return null;
+        }
+    }
+}
