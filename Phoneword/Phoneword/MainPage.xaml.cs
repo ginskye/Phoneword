d3b5598c04033df9e55826b16af243e76c48ef0b
Emily Hall <bibliomaniac@gmail.com>
2022-03-22 21:19:57 -0400

    added click handling methods, dialing and exception handling, with comments.  Background/button colors

--- Phoneword/Phoneword/MainPage.xaml.cs
+++ Phoneword/Phoneword/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
+using Xamarin.Essentials; //this library allows us to use the dialer
 
 namespace Phoneword
 {
@@ -15,8 +16,9 @@ namespace Phoneword
         public MainPage()
         {
             this.Padding = new Thickness(20, 20, 20, 20); //sets padding for the page
+            this.BackgroundColor = Color.DeepPink; //color change
 
-            StackLayout panel = new StackLayout
+            StackLayout panel = new StackLayout // this defaults to vertical, since nothing is selected.  could make horizontal by choosing Orentation = StackOrientation.Horizontal on l 21
             {
                 Spacing = 15 //establishes layout
             };
@@ -27,24 +29,31 @@ namespace Phoneword
                 FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label))
             });
 
-            panel.Children.Add(phoneNumberText = new Entry
+            panel.Children.Add(phoneNumberText = new Entry //entry field
             {
-                Text = "1-855-XAMARIN",
+                Text = "1-855-XAMARIN", //prefilled text
             });
 
-            panel.Children.Add(translateButton = new Button
+            panel.Children.Add(translateButton = new Button 
             {
                 Text = "Translate",
+                TextColor = Color.White,
+                BackgroundColor = Color.Purple,
                 
             });
 
-            translateButton.Clicked += OnTranslate;
+            translateButton.Clicked += OnTranslate; //click handling for translateButton calls OnTranslate func, l 53
+            
 
             panel.Children.Add(callButton = new Button()
             {
                 Text = "Call",
                 IsEnabled = false,
+                BackgroundColor = Color.BlueViolet,
+                TextColor = Color.White,
             });
+
+            callButton.Clicked += OnCall; //click handling for Call button calls OnCall function l.72
 
             this.Content = panel;
 
@@ -52,15 +61,46 @@ namespace Phoneword
         private void OnTranslate(object sender, EventArgs e)
         {
             string enteredNumber = phoneNumberText.Text;
-            translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber); //calls the ToNumber function from the PhonewordTranslator module
+            translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber); //calls the ToNumber method from the PhonewordTranslator module
 
-            if (!string.IsNullOrEmpty(translatedNumber))
+            if (!string.IsNullOrEmpty(translatedNumber)) //checks for content
             {
-
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + translatedNumber;
             }
             else
             {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
 
+        async void OnCall(object sender, System.EventArgs e) //async keyword goes with await
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                //dialphone
+                try
+                {
+                    PhoneDialer.Open(translatedNumber);
+                }
+                catch (ArgumentNullException) //error handling for not valid or empty
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                 }
+                catch (FeatureNotSupportedException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing not supported.", "OK");
+                }
+                catch (Exception)
+                {
+                    //another error
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
             }
         }
     }
